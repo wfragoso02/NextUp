0c809a747f1b9f702b0107a1f9e5de771ed03ee1
Wellington Fragoso <wellingtonfragoso2@gmail.com>
2019-03-26 23:10:39 -0400

    Fix minor changes on Splash page and Finish a skeleton of Session form

--- frontend/components/session/signup_form.jsx
+++ frontend/components/session/signup_form.jsx
@@ -1,16 +1,20 @@
 import { connect } from 'react-redux';
 import { signup } from '../../actions/session_actions';
 import SessionForm from './session_form';
+import { Link } from 'react-router-dom';
+import React from 'react';
+
 
 const msp = (state, ownProps) => {
     return {
         errors: state.errors.session || [],
-        formType: 'signup'
+        formType: 'Sign Up'
     };
 };
 const mdp = (dispatch) => {
     return {
-        processForm: (formUser) => dispatch(signup(formUser))
+        processForm: (formUser) => dispatch(signup(formUser)),
+        formText: () => (<p>Already a Member? <Link to="/login">Sign In</Link></p>)
     };
 };
 
