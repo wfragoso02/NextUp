7f0b5b16413fe4513925d0737f9b667a87427344
Wellington Fragoso <wellingtonfragoso2@gmail.com>
2019-04-03 10:46:42 -0400

    Chnages for Heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
 
 User.destroy_all
 Profile.destroy_all
@@ -15,14 +16,14 @@ ListItem.destroy_all
 
 
 User.create!({email: 'faker@gmail.com', password: 'password'})
-marcus = Profile.create!({name: 'Marcus', user_id: User.all.last.id, image_url: '/assets/marcus.png'})
-nicolas = Profile.create!({name: 'Nicolas', user_id: User.all.last.id ,image_url: '/assets/nicolas.png'})
+marcus = Profile.create!({name: 'Marcus', user_id: User.all.last.id, image_url: 'https://s3.amazonaws.com/nextup-seed/marcus.png'})
+nicolas = Profile.create!({name: 'Nicolas', user_id: User.all.last.id ,image_url: 'https://s3.amazonaws.com/nextup-seed/nicolas.png'})
 marvel = Video.create!({title: 'Avengers End Game', year: 2019, rating: 'PG-13', description: 'Greatest Film of All time'})
-marvel.image.attach(io: File.open('app/assets/images/avenger_end_game.jpg'), filename: 'avengers.jpg')
-marvel.movie.attach(io: File.open('Marvel Studios Avengers Endgame - Official Trailer.mp4'), filename: 'avengers-trailer.mp4')
+marvel.image.attach(io: open('https://s3.amazonaws.com/nextup-seed/avenger_end_game.jpg'), filename: 'avengers.jpg')
+marvel.movie.attach(io: open('https://s3.amazonaws.com/nextup-seed/Marvel+Studios+Avengers+Endgame+-+Official+Trailer+copy.mp4'), filename: 'avengers-trailer.mp4')
 dc = Video.create!({title: 'Justice League', year: 2018, rating: 'PG-13', description: 'Sucky Film'})
-dc.image.attach(io: File.open('app/assets/images/justice_league.jpg'), filename: 'dc.jpg')
-dc.movie.attach(io: File.open('JUSTICE LEAGUE - Official Heroes Trailer copy.mp4'), filename: 'dc-trailer.mp4')
+dc.image.attach(io: open('https://s3.amazonaws.com/nextup-seed/justice_league.jpg'), filename: 'dc.jpg')
+dc.movie.attach(io: open('https://s3.amazonaws.com/nextup-seed/JUSTICE+LEAGUE+-+Official+Heroes+Trailer.mp4'), filename: 'dc-trailer.mp4')
 
 ['Action','Movies','superHero','Ultimate Movies'].to_a.each do |alpha|
     alpha = Genre.create!({name: alpha})
