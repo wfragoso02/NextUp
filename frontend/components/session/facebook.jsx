cb312b21389dd87fdcef882dd18eb1acbaf9bb63
wfragoso02 <wellingtonfragoso2@gmail.com>
2019-05-15 10:58:20 -0400

    Facebook Auth refactor

--- frontend/components/session/facebook.jsx
+++ frontend/components/session/facebook.jsx
@@ -1,44 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FacebookLogin from 'react-facebook-login';
 
-export default class Facebook extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            isLoggedIn: false,
-        };
-        this.componentClicked = this.componentClicked.bind(this);
-        this.responseFacebook = this.responseFacebook.bind(this);
+const Facebook = ({login, signup}) => {
+
+    const [loggedIn] = useState(false)
+
+    const componentClicked = object => {
+        login(object).then( null, () => signup(object));
     }
 
-    componentClicked(object) {
-        this.props.login(object)
-        .then( null, () => this.props.signup(object));
+    const responseFacebook = (response) => {
+        componentClicked({ email: response.email, password: response.userID })
     }
 
-    responseFacebook(response){
-        this.componentClicked({ email: response.email, password: response.userID })
-    }
-
-    render(){
-        let fbContent;
-        if (this.state.isLoggedIn){
-            fbContent = null;
-        }else{
-            fbContent = (
-                <FacebookLogin
-                    appId="358268101562524"
-                    autoLoad={false}
-                    fields="email"
-                    callback={this.responseFacebook} 
-                    className="fb-logo"/> 
-            )
-        }
-
-        return(
-            <div>
-                {fbContent}
-            </div>
+    let fbContent;
+    if (loggedIn){
+        fbContent = "";
+    }else{
+        fbContent = (
+            <FacebookLogin
+                appId="358268101562524"
+                autoLoad={false}
+                fields="email"
+                callback={responseFacebook} 
+                className="fb-logo"
+            /> 
         )
     }
+
+    return(
+        <div>
+            {fbContent}
+        </div>
+    )
 }
+
+export default Facebook;
