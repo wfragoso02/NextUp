c07f76d1c6f2900d989a1473ab6778e633d5addf
wfragoso02 <wellingtonfragoso2@gmail.com>
2019-07-19 01:03:13 -0400

    minor refactor

--- frontend/components/session/login_form_container.js
+++ frontend/components/session/login_form_container.js
@@ -15,7 +15,7 @@ const msp = (state, ownProps) => {
 const mdp = (dispatch) => {
     return {
         processForm: (formUser) => dispatch(login(formUser)),
-        formText: () => (<p className="checkmark">New to Next Up? <Link className="session-a"to="/signup">Sign up now.</Link></p>),
+        formText: () => (<p className="form-text">New to Next Up? <Link className="session-a"to="/signup">Sign up now.</Link></p>),
         clearErrors: () => dispatch(clearErrors())
     };
 };
