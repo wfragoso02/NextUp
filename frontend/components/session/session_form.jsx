3cb341789a74b0c462bc5e90ac823314459e1422
wfragoso02 <wellingtonfragoso2@gmail.com>
2019-07-23 21:01:05 -0400

    refactor session form css

--- frontend/components/session/session_form.jsx
+++ frontend/components/session/session_form.jsx
@@ -1,128 +1,115 @@
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import React from 'react';
 import { withRouter } from 'react-router-dom';
 import Footer from '../footer';
 import Facebook from './facebook_container';
 
-class SessionForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: '',
-      password: '',
-      checked: '',
-      errorsEmail: '',
-      errorsPassword: ''
-    };
+const SessionForm = props => {
+  const [state, setState] = useState({
+    email: '',
+    password: '',
+    checked: '',
+    errorsEmail: '',
+    errorsPassword: ''
+  });
 
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleInput = this.handleInput.bind(this);
-    this.validateEmail = this.validateEmail.bind(this);
-  }
-
-  update() {
+  const update = () => {
     return () => {
-      if (this.state.checked === '') {
-        this.setState({ checked: 'checked' });
+      if (state.checked === '') {
+        setState({ ...state, checked: 'checked' });
       } else {
-        this.setState({ checked: '' });
+        setState({ ...state, checked: '' });
       }
     };
-  }
+  };
 
-  componentDidMount() {
-    this.props.clearErrors();
-  }
+  useEffect(() => {
+    props.clearErrors();
+  }, []);
 
-  validateEmail(email){
+  const validateEmail = (email) => {
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
-  }
+  };
 
-  handleInput(type) {
+  const handleInput = (type) => {
     return (e) => {
       if (type === 'password' && (e.target.value.length < 4 || e.target.value.length > 60)) {
-        this.setState({ errorsPassword: 'Your password must contain between 4 and 60 characters.' });
+        setState({ ...state, errorsPassword: 'Your password must contain between 4 and 60 characters.', [type]: e.target.value });
       } else if ((e.target.value.length >= 4 && e.target.value.length <= 60)) {
-        this.setState({ errorsPassword: '' });
+        setState({ ...state, errorsPassword: '', [type]: e.target.value });
       }
 
       if (type === 'email') {
-        if (this.validateEmail(e.target.value)){
-          this.setState({ errorsEmail: '' });
-        }else{
-          this.setState({ errorsEmail: 'Please enter a valid email address' });
+        if (validateEmail(e.target.value)) {
+          setState({ ...state, errorsEmail: '', [type]: e.target.value });
+        } else {
+          setState({ ...state, errorsEmail: 'Please enter a valid email address', [type]: e.target.value });
         }
       }
-
-      this.setState({ [type]: e.target.value });
     };
+  };
 
-  }
-
-  handleSubmit(e) {
+  const handleSubmit = (e) => {
     e.preventDefault();
+    props.processForm(state);
+  };
 
-    this.props.processForm(this.state).then(() => this.props.history.push('/home'));
+  const sessionErrors = props.errors.map((error, idx) => {
+    return (
+      <li key={idx} className="error">{error}</li>
+    )
+  })
+  let a = "";
+  let b = "";
+  if (state.errorsEmail.length > 0) {
+    a = "form-input-with-email-error"
+  } else {
+    a = 'form-input'
+  }
+  if (state.errorsPassword.length > 0) {
+    b = "form-input-with-password-error"
+  } else {
+    b = 'form-input'
   }
 
-  render() {
-    const sessionErrors = this.props.errors.map((error, idx) => {
-      return (
-        <li key={idx} className="error">{error}</li>
-      )
-    })
-    let a = "";
-    let b = "";
-    if (this.state.errorsEmail.length > 0) {
-      a = "form-input-with-email-error"
-    } else {
-      a = 'form-input'
-    }
-    if (this.state.errorsPassword.length > 0) {
-      b = "form-input-with-password-error"
-    } else {
-      b = 'form-input'
-    }
-
-    return (
-      <div className="sessions-form">
-        <div className="temp">
-          <Link to="/"><img src={window.logo} className="logo1" /></Link>
-          <div className="session-body">
-            <div className="display-form">
-              <h1>{this.props.formType}</h1>
-              <form >
-                {sessionErrors}
-                <br />
-                <input className={a} name={a} type="text" value={this.state.email} onChange={this.handleInput('email')} required/>
-                <label className="form-input-label" htmlFor={a}>Email</label>
-                <h3 className="session-errors">{this.state.errorsEmail}</h3>
-                <br />
-                <input className={b} name={b} type="password" value={this.state.password} onChange={this.handleInput('password')} required/>
-                <label className="form-input-label" htmlFor={b}>Password</label>
-                <h3 className="session-errors">{this.state.errorsPassword}</h3>
-                <br />
-                <button className="session-button" onClick={this.handleSubmit}>{this.props.formType}</button>
-              </form>
-              <div className="form-sub">
-                <label className='remember-me'>Remember me
-                  <input type="checkbox" checked={this.state.checked} onChange={this.update()} />
-                  <span className="checkmark"></span>
-                </label>
-                <a className="sub-a" href="">Need help?</a>
-              </div>
-            </div>
-            <Facebook processForm={this.props.processForm} />
-            <div className="session-bottom">
-              {this.props.formText()}
+  return (
+    <div className="sessions-form">
+      <div className="temp">
+        <Link to="/"><img src={window.logo} className="logo1" /></Link>
+        <div className="session-body">
+          <div className="display-form">
+            <h1>{props.formType}</h1>
+            <form >
+              {sessionErrors}
+              <br />
+              <input className={a} name={a} type="text" value={state.email} onChange={handleInput('email')} required />
+              <label className="form-input-label" htmlFor={a}>Email</label>
+              <h3 className="session-errors">{state.errorsEmail}</h3>
+              <br />
+              <input className={b} name={b} type="password" value={state.password} onChange={handleInput('password')} required />
+              <label className="form-input-label" htmlFor={b}>Password</label>
+              <h3 className="session-errors">{state.errorsPassword}</h3>
+              <br />
+              <button className="session-button" onClick={handleSubmit}>{props.formType}</button>
+            </form>
+            <div className="form-sub">
+              <label className='remember-me'>Remember me
+                  <input type="checkbox" checked={state.checked} onChange={update()} />
+                <span className="checkmark"></span>
+              </label>
+              <a className="sub-a" href="">Need help?</a>
             </div>
           </div>
+          <Facebook processForm={props.processForm} />
+          <div className="session-bottom">
+            {props.formText()}
+          </div>
         </div>
-        <Footer />
       </div>
-    )
-  }
+      <Footer />
+    </div>
+  )
 }
 
 export default withRouter(SessionForm);
