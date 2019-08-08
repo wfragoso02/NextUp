1d91706bdcc551fa1f2ec10433a955260d3c2fe9
wfragoso02 <wellingtonfragoso2@gmail.com>
2019-08-08 08:35:46 -0400

    final redux refactor

--- frontend/components/genres/genre_container.js
+++ frontend/components/genres/genre_container.js
@@ -1,5 +1,5 @@
 import { connect } from 'react-redux';
-import GenreIndex from './genre_index2';
+import GenreIndex from './genre_index';
 import { fetchGenres } from '../../actions/genre_actions';
 import { fetchProfile } from '../../actions/profile_actions';
 import { fetchList } from '../../actions/list_actions';
--- frontend/components/genres/genre_index.jsx
+++ frontend/components/genres/genre_index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import GenreIndexItem from './genre_index_item';
 import Nav from '../nav/nav_cotainer';
 import VideoIndexItem from '../videos/video_index_item';
@@ -8,220 +8,163 @@ import InfiniteScroll from 'react-infinite-scroll-component';
 import GenreContent from './genre_content';
 import Arrow from '../arrow';
 
+const GenreIndex = props => {
+  const [volume, changeVolume] = useState('1');
+  const [start, changeStart] = useState(0);
+  const [count, changeCount] = useState(1);
+  const [genres, changeGenres] = useState([]);
+  const [selectedItem, changeSelectedItem] = useState(null);
+  const [length, changeLength] = useState(0);
+  const [shift, changeShift]  = useState(0);
+  const [promoVideo, changePromoVideo] = useState(props.promoVid);
+  const [contentVideos, changeContentVideos] = useState({});
+  const videoRef = React.createRef();
 
-class GenreIndex extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      volume: "1",
-      start: 0,
-      count: 1,
-      genres: [],
-      selectedItem: null,
-      length: 0,
-      shift: 0,
-      promoVideo: this.props.promoVid
-    };
+  const onlyUnique = (value, index, self) => {
+    return self.indexOf(value) === index;
+  };
 
-    this.updateCount = this.updateCount.bind(this);
-    this.setMuted = this.setMuted.bind(this);
-    this.selectListItem = this.selectListItem.bind(this);
-    this.closeContent = this.closeContent.bind(this);
-    this.changeShift = this.changeShift.bind(this);
-  }
-
-  changeShift(num){
-    this.setState({ shift: num});
-  }
-
-  componentDidMount() {
-    this.props.fetchVideos().then(() => {
-      const promoVideo = Object.values(this.props.all_videos)[Math.floor(Math.random() * Object.values(this.props.all_videos).length)];
-      if(this.state.promoVideo === undefined){
-        this.setState({promoVideo: promoVideo});
-      }
+  useEffect(() => {
+    props.fetchVideos().then((res) => {
+      const promoVid = Object.values(res.videos)[Math.floor(Math.random() * Object.values(res.videos).length)];
+      if(typeof promoVideo === 'undefined') changePromoVideo(promoVid);
     });
-    this.props.fecthProfile(this.props.match.params.profileId);
-    this.props.fetchGenres().then((res) => {
-      function onlyUnique(value, index, self) {
-        return self.indexOf(value) === index;
-      }
-      this.setState({ genres: Object.values(res.genres).slice(0, 1), length: this.props.list.list_video_ids.filter(onlyUnique).length - 5 });
+    props.fetchGenres().then((res) => {
+      changeGenres(Object.values(res.genres).slice(0, 1));
     });
-  }
+  },[]);
 
-  componentDidUpdate(prevProps) {
-    if (prevProps.profile.id !== this.props.profile.id) {
-      this.setState({selectedItem: null});
-      this.props.fecthProfile(this.props.match.params.profileId).then(() => this.props.fetchList(this.props.profile.list.id)).then(() => this.props.fetchVideos());
-    }
-    if (prevProps.list.list_video_ids && (this.props.list.list_video_ids.length !== prevProps.list.list_video_ids.length)) {
-      function onlyUnique(value, index, self) {
-        return self.indexOf(value) === index;
-      }
-      this.setState({ length: this.props.list.list_video_ids.filter(onlyUnique).length - 5 });
-    }
-  }
+  useEffect(() => {
+    changeSelectedItem(null);
+    props.fetchProfile(props.match.params.profileId).then((res) => {
+      changeLength(res.payload.list.list_video_ids.length - 5);
+    });
+  }, [props.match.params.profileId]);
 
-  updateCount() {
+  useEffect(() => {
+    if(props.list.list_video_ids) changeLength(props.list.list_video_ids.length - 5);
+  }, [props.list]);
+
+
+  const updateCount = () => {
     setTimeout(() => {
-      this.setState({ start: this.state.count, count: this.state.count + 1, genres: this.state.genres.concat(this.props.genres.slice(this.state.count, this.state.count + 1)) });
-    }, 700);
-  }
+      changeStart(count);
+      changeCount(count + 1);
+      changeGenres(genres.concat(props.genres.slice(count, count + 1)));
+    }, 500);
+  };
 
-  setMuted() {
-    if (this.refs.player.muted) {
-      this.refs.player.muted = false;
-      this.setState({ volume: 1 });
-    } else {
-      this.refs.player.muted = true;
-      this.setState({ volume: 0 });
-    }
-  }
+  const setMuted = (newVolume) => {
+    videoRef.current.muted = videoRef.current.muted ? false : true;
+    changeVolume(newVolume);
+  };
 
-  closeContent(id = null) {
-    if (id) {
-      this.setState({ [id]: null });
-    } else {
-      this.setState({ selectedItem: null });
-    }
-  }
+  const closeContent = (id = null) => {
+    id ? changeContentVideos({...contentVideos, [id]: null}) : changeSelectedItem(null) ;
+  };
 
-  selectListItem(video, id = null) {
-    if (id) {
-      this.setState({ [id]: video });
-    } else {
-      this.setState({ selectedItem: video });
-    }
-  }
+  const selectListItem = (video, id = null) => {
+    id ? changeContentVideos({...contentVideos, [id]: video}) : changeSelectedItem(video) ;
+  };
 
-  render() {
-    if (!this.props.list || Object.values(this.props.all_videos).length < 1 || this.state.promoVideo === undefined ) return null;
-    const genres = this.state.genres.map((genre, idx) => {
-      return (
-        <>
-          <li key={`${genre.id}-${idx}`}>
-            <GenreIndexItem
-              selectListItem={this.selectListItem}
-              currVid={this.state[genre.id]}
-              genre={genre} />
-          </li>
-          <GenreContent id={genre.id} video={this.state[genre.id]} closeContent={this.closeContent} />
-        </>
-      )
-    });
-
-    let defaultButton = "";
-    // if (!this.props.list.list_video_ids || !this.props.genres[0]) {
-    //   return null;
-    // }
-
-    this.props.list.list_video_ids.includes(this.state.promoVideo.id) ?
-      defaultButton = (<button onClick={() => this.props.deleteListItem({ video_id: this.state.promoVideo.id, list_id: this.props.list.id })} className="front-page-button"><h3 className="fa-check-text"><i className="fas fa-check"></i>My List </h3></button>)
-      :
-      defaultButton = (<button onClick={() => this.props.createListItem({ video_id: this.state.promoVideo.id, list_id: this.props.list.id })} className="front-page-button"><h3 className="fa-plus-text"><i className="fas fa-plus"></i>My List</h3></button>)
-      ;
-
-    let volumes;
-    if (this.state.volume === 0) {
-      volumes = <i className="fas fa-volume-mute fa-xs"></i>
-    } else {
-      volumes = <i className="fas fa-volume-up fa-xs"></i>
-    }
-    let mainVideo;
-    if (Object.values(this.props.all_videos).length > 0) {
-      mainVideo = (
-        <>
-          <video ref='player' className="home-trailer" loop autoPlay>
-            <source src={this.state.promoVideo.video_url} />
-          </video>
-          <div className={`main-video-title ${this.state.promoVideo.title.length > 25 ? "large-title": "small-title"}`}>
-            <h1 >{this.state.promoVideo.title}</h1>
-            <div className="genre-content-play-and-list">
-              <Link to={`/${this.props.profile.id}/videos/${this.state.promoVideo.id}`} className="play-button"><h3>► Play</h3></Link>
-              {defaultButton}
-            </div>
-          </div>
-          <button onClick={this.setMuted} className="home-page-volume-button">{volumes}</button>
-          <h1 className="main-video-rating">{this.state.promoVideo.rating}</h1>
-        </>
-      )
-
-    } else {
-      mainVideo = (
-        <h1>No Video Here</h1>
-      )
-    }
-
-    let myList;
-
-    let listVideos;
-    myList = <div className="genre-index-links"><h2 className="genre-content"><Link to={`/${this.props.profile.id}/myList`} >My List</Link></h2></div>
-    if (!this.props.list.list_video_ids) {
-      return null;
-    } else {
-      function onlyUnique(value, index, self) {
-        return self.indexOf(value) === index;
-      }
-      listVideos = this.props.list.list_video_ids.filter(onlyUnique).map(video_id => {
-        return (
-          <VideoIndexItem
-            classId={this.props.list.id}
-            currVid={this.state.selectedItem}
-            selectListItem={this.selectListItem}
-            profile={this.props.profile}
-            deleteListItem={this.props.deleteListItem}
-            list={this.props.list}
-            createListItem={this.props.createListItem}
-            video={this.props.all_videos[video_id]}
-            className="actual-video" />
-        )
-      })
-    }
-    let arrowLeft;
-    this.state.shift > 0 ?
-      arrowLeft = (
-        <Arrow direction='left' shift={this.state.shift} id={this.props.list.id} changeShift={this.changeShift} />
-      ) : arrowLeft = null;
-
-    let arrowRight
-    this.state.shift < this.state.length ?
-      arrowRight = (
-        <Arrow direction='right' shift={this.state.shift} id={this.props.list.id} changeShift={this.changeShift} />
-      ) : arrowRight = null;
-
+  if (!props.list || Object.values(props.all_videos).length < 1 || typeof promoVideo === 'undefined' ) return null;
+  const allGenres = genres.map((genre, idx) => {
     return (
-      <div className="genre-index-container">
-        <Nav profileId={this.props.profile.id} />
-        <div className="home" >
-          {mainVideo}
-        </div>
-        <div>
-          {myList}
-          <br />
-          <ul className="row">
-            {arrowLeft}
-            {listVideos}
-            {arrowRight}
-          </ul>
-
-          <GenreContent id={null} video={this.state.selectedItem} closeContent={this.closeContent} />
-        </div>
-        <br />
-
-        <InfiniteScroll
-          dataLength={this.state.count}
-          next={this.updateCount}
-          hasMore={true}
-        >
-          <ul className="genre-index">
-            {genres}
-          </ul>
-        </InfiniteScroll>
-        <Footer />
-      </div>
+      <>
+        <li key={`${genre.id}-${idx}`}>
+          <GenreIndexItem
+            selectListItem={selectListItem}
+            currVid={contentVideos[genre.id]}
+            genre={genre} />
+        </li>
+        <GenreContent id={genre.id} video={contentVideos[genre.id]} closeContent={closeContent} />
+      </>
     )
-  }
-}
+  });
+
+  const defaultButton = props.list.list_video_ids.includes(promoVideo.id) ?
+    (<button onClick={() => props.deleteListItem({ video_id: promoVideo.id, list_id: props.list.id })} className="front-page-button"><h3 className="fa-check-text"><i className="fas fa-check"></i>My List </h3></button>)
+  :
+    (<button onClick={() => props.createListItem({ video_id: promoVideo.id, list_id: props.list.id })} className="front-page-button"><h3 className="fa-plus-text"><i className="fas fa-plus"></i>My List</h3></button>)
+  ;
+
+  const newVolume = volume === 0 ? 1 : 0;
+  const videoVolume = volume === 0 ?
+    <i className="fas fa-volume-mute fa-xs"></i>
+  :
+    <i className="fas fa-volume-up fa-xs"></i>
+  ;
+
+  const mainVideo = (
+    <>
+      <video ref={videoRef} className="home-trailer" loop autoPlay>
+        <source src={promoVideo.video_url} />
+      </video>
+      <div className={`main-video-title ${promoVideo.title.length > 25 ? "large-title": "small-title"}`}>
+        <h1 >{promoVideo.title}</h1>
+        <div className="genre-content-play-and-list">
+          <Link to={`/${props.profile.id}/videos/${promoVideo.id}`} className="play-button"><h3>► Play</h3></Link>
+          {defaultButton}
+        </div>
+      </div>
+      <button onClick={() => setMuted(newVolume)} className="home-page-volume-button">{videoVolume}</button>
+      <h1 className="main-video-rating">{promoVideo.rating}</h1>
+    </>
+  )
+
+  const arrowLeft = shift <= 0 ? null :
+    <Arrow direction='left' shift={shift} id={props.list.id} changeShift={changeShift} />
+  ;
+
+  const arrowRight = shift >= length ? null : 
+    <Arrow direction='right' shift={shift} id={props.list.id} changeShift={changeShift} />
+  ;
+  
+  const myList = <div className="genre-index-links"><h2 className="genre-content"><Link to={`/${props.profile.id}/myList`} >My List</Link></h2></div>
+  const listVideos = props.list.list_video_ids.filter(onlyUnique).map(video_id => {
+    return (
+      <VideoIndexItem
+        classId={props.list.id}
+        currVid={selectedItem}
+        selectListItem={selectListItem}
+        profile={props.profile}
+        deleteListItem={props.deleteListItem}
+        list={props.list}
+        createListItem={props.createListItem}
+        video={props.all_videos[video_id]}
+        className="actual-video" />
+    )
+  })
+
+  return (
+    <div className="genre-index-container">
+      <Nav profileId={props.profile.id} />
+      <div className="home" >
+        {mainVideo}
+      </div>
+      <div>
+        {myList}
+        <br />
+        <ul className="row">
+          {arrowLeft}
+          {listVideos}
+          {arrowRight}
+        </ul>
+        <GenreContent id={null} video={selectedItem} closeContent={closeContent} />
+      </div>
+      <br />
+      <InfiniteScroll
+        dataLength={count}
+        next={updateCount}
+        hasMore={true}
+      >
+        <ul className="genre-index">
+          {allGenres}
+        </ul>
+      </InfiniteScroll>
+      <Footer />
+    </div>
+  )
+};
 
 export default GenreIndex;
